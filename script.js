954f5293a05c5f9cda7dd9d9224026773cc63678
LimaCarlosSantana <calos.lima.edu@gmail.com>
2024-06-22 08:44:12 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
+ 
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu')
+ 
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('show-icon')
+    })
+ }
+ 
+ showMenu('nav-toggle','nav-menu')
+
+/*=============== SLIDER ===============*/
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
+let slider = document.querySelector('.slider');
+
+next.addEventListener('click', function(){
+    let slides = document.querySelectorAll('.slides');
+    slider.appendChild(slides[0]);
+})
+prev.addEventListener('click', function(){
+    let slides = document.querySelectorAll('.slides');
+    slider.prepend(slides[slides.length - 1]);
+})
